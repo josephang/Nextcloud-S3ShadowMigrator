b3de6c5fccc74c25f97d39387d9c9bfb3d1035c5
josephang <jang@originhangar.com>
2026-05-04 18:51:13 -0700

    fix: bump max-version to 33, add composer autoloader

--- appinfo/app.php
+++ appinfo/app.php
@@ -5,5 +5,9 @@ declare(strict_types=1);
 $appManager = \OC::$server->getAppManager();
 $appManager->registerApp('s3shadowmigrator');
 
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
+
 // Note: Bootstrapping and middleware registration will be moved to lib/AppInfo/Application.php
 // which implements IBootstrap.
